510a7d76d4f017d26a007d900a2b2b83e96c5e7f
Priyansh Bhardwaj <priyansh.bhardwaj@metacube.com>
2020-02-07 22:00:28 +0530

    EAD Assignment5

---
+++ EAD/Assignment6/shoppingcart/dao/CartOperations.java
@@ -0,0 +1,52 @@
+package com.metacube.shoppingcart.dao;
+
+import java.util.Map;
+import com.metacube.shoppingcart.model.Product;
+import com.metacube.shoppingcart.model.User;
+
+/**
+ * @author krishan
+ * CartOperations interface has the methods of Cart Operations
+ *
+ */
+public interface CartOperations
+{
+
+	/**
+	 * @param productName
+	 * @param quantity
+	 * Adds Product to Cart
+	 */
+	public void addProduct(String productName, int quantity);
+
+	/**
+	 * @param productName
+	 * @param quantity
+	 * Deletes product from Cart
+	 */
+	public void deleteProduct(String productName, int quantity);
+
+	/**
+	 * @return Items
+	 * Displays Cart
+	 */
+	public Map<Product, Integer> showCart();
+
+	/**
+	 * @return total Bill
+	 * Displays Bill
+	 */
+	public double showBill();
+
+	/**
+	 * @param user
+	 * Adds User
+	 */
+	public void setUser(User user);
+
+	/** 
+	 * @return user
+	 * Displays User of a Cart
+	 */
+	public User showUser();
+}
